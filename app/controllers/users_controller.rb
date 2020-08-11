a78c19b037fa30214f4e89ca1332bea126437155
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-11 16:19:23 -0500

    Adding create action and authentification_token input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    # when we submit the form in users/new.html.erb we activate users#create action
+    # we create a new instance of user by passing params get from the form
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # we save the user instance! If its valid (according to validates rules in user model app/models/user.rb) we get to new_user_path else we get to URL/users
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates :username, presence: true
-  validate :email, presence: true
-  vaidate: password, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
