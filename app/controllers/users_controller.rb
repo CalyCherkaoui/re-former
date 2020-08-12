89837ddb3d7784e884e81dba821867dd85c55e59
houda cherkaoui <houda.cherkaoui@gmail.com>
2020-08-12 13:08:03 -0500

    Adding editing/updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,20 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
